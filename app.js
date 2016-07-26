69503e06c29ae5ad82bf878ab8103cca35710dc5
Daniel <danielcomriebell@gmail.com>
2016-07-26 15:08:46 -0500

    Working Dynamic NodeJS Website

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var router = require("./router.js");
 // Create a web server
 var http = require('http');
 http.createServer(function(req,res){
-  // router.home(req, res);
+  router.home(req, res);
   router.user(req,res);
 }).listen(3000, '127.0.0.1');
 console.log("Server running at http://127.0.0.1:3000/");
--- router.js
+++ router.js
@@ -1,15 +1,35 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
-
+var querystring = require("querystring");
 var commonHeaders = {'Content-Type': 'text/html'};
 
 // Handle the HTTP route GET / and POST / i.e Home
 function home(req, res){
-    res.writeHead(200, commonHeaders);
+  if (req.url === "/"){
+    if (req.method.toLowerCase() === "get"){
+
+      //show search
+      res.writeHead(200, commonHeaders);
       renderer.view("header", {}, res);
       renderer.view("search", {}, res);
       renderer.view("footer", {}, res);
-}
+    } else {
+      //if url == '/' && POST
+        //get the post data from the body
+        req.on("data", function(postBody){
+          // console.log(postBody.toString());
+
+          //extract the username
+          var query = querystring.parse(postBody.toString());
+          res.writeHead(303, {"Location": "/" + query.username});
+          res.end();
+        });
+        //extract the username
+        //redirect to the username
+
+    }
+  }
+};
 
 //Handle the HTTP route GET /:username i.e. /chalkers
 function user(req, res){
